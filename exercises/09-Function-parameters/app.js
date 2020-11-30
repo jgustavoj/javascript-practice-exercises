3600ec6ec172971a434c5b7a96f69ac73cbf3e60
jgustavoj <gjime011@fiu.edu>
2020-11-30 18:55:05 +0000

    practice

--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,5 +1,7 @@
 // Your code goes here:
-
+ function renderPerson(name, dateOfBirth, color, integer, gender) {
+    return name +  " is a " + integer + " years old " + gender + " born on " + dateOfBirth + " with " + color + " eyes"
+};
 
 
   ///*** Do not edit below this line ****/
