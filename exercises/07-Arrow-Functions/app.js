bc12f38192bee3639c3639f5d467333a7214daa5
jgustavoj <gjime011@fiu.edu>
2020-11-30 18:30:29 +0000

    practice

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,6 +1,4 @@
-let rapid = (i) => {for (i = 0; i < i.length; i++);}
-        // if(i == ['O']) {
-        //     i.replace('O','')};
+const rapid = () => {for (let i=0; i < i.length; i++);} 
 
 
 
@@ -10,5 +8,18 @@ let rapid = (i) => {for (i = 0; i < i.length; i++);}
 
 
 
- //str.toUpperCase()
-//    'a','e','i','o','u'
+   
+// let rapid = (i) => {for (i = 0; i < i.length; i++);}
+//     // if(i == ['O']) {
+//     //     i.replace('O','')};
+
+
+
+// // From this line up Do not change code below
+// let str = "John";
+// console.log(rapid(str));
+
+
+
+// //str.toUpperCase()
+// //    'a','e','i','o','u'
