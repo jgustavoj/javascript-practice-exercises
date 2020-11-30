573e0f85e1c9156500088d080b1e987a5dd5ff38
jgustavoj <gjime011@fiu.edu>
2020-11-30 22:43:27 +0000

    exercise

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,25 +1,14 @@
-const rapid = () => {for (let i=0; i < i.length; i++);} 
-
-
+const rapid = (param) => {
+    let newString = "";
+    for (let i=0; i < param.length; i++){
+        param[i].toLowerCase(); 
+        if (param[i] != "a" && param[i] != "e" && param[i] != "i" && param[i] != "o" && param[i] != "u"){
+            newString += param[i].toUpperCase(); 
+        }
+    }
+return newString
+}
 
    // From this line up Do not change code below
    let str = "John";
    console.log(rapid(str));
-
-
-
-   
-// let rapid = (i) => {for (i = 0; i < i.length; i++);}
-//     // if(i == ['O']) {
-//     //     i.replace('O','')};
-
-
-
-// // From this line up Do not change code below
-// let str = "John";
-// console.log(rapid(str));
-
-
-
-// //str.toUpperCase()
-// //    'a','e','i','o','u'
--- exercises/10-Array-Methods/app.js
+++ exercises/10-Array-Methods/app.js
@@ -1,7 +1,7 @@
 var names = ['John', 'Kenny', 'Tom', "Bob", 'Dilan'];
 
-function sortNames(){
-
+function sortNames(param){
+    return param.sort()
 }
 
 console.log(sortNames(names));
